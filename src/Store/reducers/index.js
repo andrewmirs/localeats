d973875dafe6c4e6ea2599e2d0bece2e311d1f80
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-02-27 22:52:33 -0800

    restyled landing page, added redux form aand basics of sign up form

--- src/Store/reducers/index.js
+++ src/Store/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import reviews from './reviews';
 
 const rootReducer = combineReducers({
+    form: formReducer,
     reviews
 });
 
