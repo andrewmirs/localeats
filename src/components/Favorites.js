0974782ea87e69622498822aa7212f9bd4909134
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-03-12 17:13:34 -0700

    Now using google predictions and options display based on current location; once you select an option the console gets the json with the place details

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,35 +1,66 @@
 import React, { Component } from 'react';
-import { Dimensions, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import MapView from 'react-native-maps';
 import { api_key } from '../../config/google_maps_api';
 import _ from 'lodash';
 
-class Profile extends Component {
+class Favorites extends Component {
    
     constructor(props){
         super(props);
 
         this.state={
             error: '',
-            latitude: 33.6846,
-            longitude: -117.8265,
-            destination: '',
-            candidates: [],
+            latitude: 0,
+            longitude: 0,
+            location: '',
+            predictions: [],
+            placeid: '',
+            details: null,
         };
-        // this.onChangeDestinationDebounced = _.debounce(this.onChangeDestination, 1000)
+        this.onChangeLocationDebounced = _.debounce(this.onChangeLocation, 1000)
     }
 
-    onChangeDestination = async (destination) => {
+    componentDidMount = () => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+            },
+            error => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, maximumAge: 2000, timeout: 20000 }
+        );
+    }
+
+    onChangeLocation = async (location) => {
         this.setState({
-            destination
+            location
         });
-        const apiUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${destination}&inputtype=textquery&fields=id,photos,formatted_address,geometry/location,name,rating&locationbias=ipbias&key=${api_key}`;
+        const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${api_key}&input=${location}&location=${this.state.latitude},${this.state.longitude}&radius=2000`;
+        try {
+            const result = await fetch(apiUrl);
+            const json = await result.json();
+            this.setState({
+                predictions: json.predictions,
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    selectLocation = async (placeid) => {
+        this.setState({
+            placeid
+        });
+        const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?key=${api_key}&placeid=${placeid}&fields=name,formatted_address,photo,website,id,formatted_phone_number`;
         try {
             const result = await fetch(apiUrl);
             const json = await result.json();
             console.log(json);
             // this.setState({
-            //     candidates: json.candidates
+            //     details: json.result,
             // })
         } catch (err) {
             console.log(err);
@@ -38,8 +69,15 @@ class Profile extends Component {
 
     render(){
 
-        const candidates = this.state.candidates.map(candidate => (
-            <Text key={candidate.id}>{candidate.name}, {candidate.formatted_address}</Text>
+        const predictions = this.state.predictions.map(( prediction, index ) => (
+            <TouchableOpacity
+                style={index == 4 ? styles.suggestionsRoundedBottom : styles.suggestions}
+                onPress={() => this.selectLocation(prediction.place_id)}
+                key={prediction.id}
+                index={index}
+            >
+                <Text >{prediction.description}</Text>
+            </TouchableOpacity>
         ));
 
         return (
@@ -52,15 +90,15 @@ class Profile extends Component {
                         latitudeDelta: 0.015,
                         longitudeDelta: 0.0121,
                     }}
-                    showUserLocation={true}
+                    showsUserLocation={true}
                 />
                 <TextInput 
                     placeholder="Search.." 
                     value={this.state.destination} 
-                    onChangeText={destination => this.onChangeDestination(destination)}
-                    style={styles.destinationInput}
+                    onChangeText={location => this.onChangeLocationDebounced(location)}
+                    style={this.state.predictions.length == 0 ? styles.locationInput : styles.locationInputWithPredictions}
                 />
-                { candidates }
+                { predictions }
             </View>
         );
     }
@@ -72,9 +110,24 @@ const styles = StyleSheet.create({
     container: {
        ...StyleSheet.absoluteFillObject
     },
-    destinationInput: {
+    locationInput: {
         height: 40,
         borderWidth: 0.5,
+        borderColor: 'darkgrey',
+        borderRadius: 5,
+        marginTop: 50,
+        marginLeft: 5,
+        marginRight: 5,
+        padding: 5,
+        backgroundColor: 'white',
+        zIndex: 5,
+    },
+    locationInputWithPredictions: {
+        height: 40,
+        borderWidth: 0.5,
+        borderColor: 'darkgrey',
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
         marginTop: 50,
         marginLeft: 5,
         marginRight: 5,
@@ -84,7 +137,28 @@ const styles = StyleSheet.create({
     },
     map: {
         ...StyleSheet.absoluteFillObject,
-    }
+    },
+    suggestions: {
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        padding: 5,
+        fontSize: 16,
+        borderWidth: 0.5,
+        marginLeft: 5,
+        marginRight: 5,
+        borderRadius: 0,
+        borderColor: 'darkgrey',
+    },
+    suggestionsRoundedBottom: {
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        padding: 5,
+        fontSize: 16,
+        borderWidth: 0.5,
+        marginLeft: 5,
+        marginRight: 5,
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+        borderColor: 'darkgrey',
+    },
 });
 
-export default Profile;
+export default Favorites;
